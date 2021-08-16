d2644987f882c4a0c8026b033594a5ff82f844bd
nihalmaredia <47981813+nihalmaredia@users.noreply.github.com>
2021-08-16 12:22:25 +0930

    Week 1 task completed (Blazor hello world app)
    Including modify the component, add counter etc.

--- Nihal (s327992)/Week 1/blazorapp/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ Nihal (s327992)/Week 1/blazorapp/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace blazorapp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 1 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 2 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 3 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 4 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 5 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 6 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 7 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 8 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 9 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using blazorapp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Nihal\blazorapp\_Imports.razor"
+#line 10 "c:\Users\Nihal\blazorapp\_Imports.razor"
 using blazorapp.Shared;
 
 #line default
@@ -91,13 +91,16 @@ using blazorapp.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\Nihal\blazorapp\Pages\Counter.razor"
+#line 9 "c:\Users\Nihal\blazorapp\Pages\Counter.razor"
        
     private int currentCount = 0;
 
+    [Parameter]
+    public int IncrementAmount { get; set; } = 1;
+
     private void IncrementCount()
     {
-        currentCount++;
+        currentCount += IncrementAmount;
     }
 
 #line default
